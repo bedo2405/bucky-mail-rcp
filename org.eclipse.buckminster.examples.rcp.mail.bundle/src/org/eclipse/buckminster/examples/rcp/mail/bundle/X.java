db17d725bc7a36de6aaec3b3dec0c922c0c4c15f
Philip Bedingfield <philip.bedingfield@gmail.com>
2014-01-25 13:02:31 +1100

    Update

--- org.eclipse.buckminster.examples.rcp.mail.bundle/src/org/eclipse/buckminster/examples/rcp/mail/bundle/X.java
+++ org.eclipse.buckminster.examples.rcp.mail.bundle/src/org/eclipse/buckminster/examples/rcp/mail/bundle/X.java
@@ -1,8 +1,12 @@
 package org.eclipse.buckminster.examples.rcp.mail.bundle;
 
-public class X {
+import org.apache.log4j.Logger;
 
+public class X {
+	private static final Logger logger = Logger.getLogger(X.class);
+	
 	public String getX() {
+		logger.trace("getX()");
 		return "X";
 	}
 	
--- org.eclipse.buckminster.examples.rcp.mail.bundle/src/org/eclipse/buckminster/examples/rcp/mail/bundle/Y.java
+++ org.eclipse.buckminster.examples.rcp.mail.bundle/src/org/eclipse/buckminster/examples/rcp/mail/bundle/Y.java
@@ -1,8 +1,15 @@
 package org.eclipse.buckminster.examples.rcp.mail.bundle;
 
+import org.apache.log4j.Logger;
+
 public class Y {
 
+	private static final Logger logger = Logger.getLogger(Y.class);
+
 	public String getY() {
+
+		logger.trace("getY()");
+
 		return "Y";
 	}
 }
